6307aecedaa92c1b276b85574e2c63249dc6bab7
emily_m <morin.emily@gmail.com>
2022-04-27 13:54:44 -0400

    Added call to API for repository info with parameters and function to display repo info

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
+// GLOBAL VARIABLES
 // selecting div class="overview" in intro section
 const overview = document.querySelector(".overview");
 
 // github username
 const username = "emily-morin";
 
-// async function to fetch username from Github profile
+// list where repo info shows up
+const repoList = document.querySelector(".repo-list");
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// FUNCTIONS
+
+// async function to fetch user data from Github profile and display it
 const usernameFetch = async function () {
     const data = await fetch(`https://api.github.com/users/${username}`);
     const cleanData = await data.json();
-    console.log(cleanData);
+    // console.log(cleanData);
     // calling displayUserData with data from API request as an argument
     displayUserData(cleanData);
-}
+};
 usernameFetch();
 
-// function to display user data from usernameFetch
+// function to display user data
 const displayUserData = function (jsonData) {
     const userInfo = document.createElement("div");
     userInfo.classList.add("user-info");
@@ -29,6 +37,23 @@ const displayUserData = function (jsonData) {
         <p><strong>Number of public repos:</strong> ${jsonData.public_repos}</p>
     </div>`
     overview.append(userInfo);
+    getRepos();
 };
 
+// async function to fetch repos, with params to sort by most recently updated & up to 100 per page
+const getRepos = async function () {
+    const data = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const cleanData = await data.json();
+    // console.log(cleanData);
+    displayRepoInfo(cleanData);
+};
 
+// function to display repo info
+const displayRepoInfo = function (repo) {
+    for (const item of repo) {
+        li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${item.name}</h3>`;
+        repoList.append(li);
+    }
+};
